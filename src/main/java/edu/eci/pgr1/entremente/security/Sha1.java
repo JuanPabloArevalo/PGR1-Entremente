d6da38c360e7be867dd0fdce1788f2bc4ab0fc3c
Administrador <Administrador@DESARROLLO1.mshome.net>
2018-04-02 17:27:45 -0500

    Adicion Clase SHA1

---
+++ src/main/java/edu/eci/pgr1/entremente/security/Sha1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pgr1.entremente.security;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Sha1 {
+
+    static String sha1(String input) {
+        MessageDigest mDigest = null;
+        try {
+            mDigest = MessageDigest.getInstance("SHA1");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+}
