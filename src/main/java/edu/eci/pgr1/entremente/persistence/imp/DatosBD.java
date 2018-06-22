a9dbc32465af78d64651a6f7b6e774c047562613
Desarrollo1 <devsolutionco@gmail.com>
2018-06-22 16:45:45 -0500

    Connection with MariaDB branch.

--- src/main/java/edu/eci/pgr1/entremente/persistence/imp/DatosBD.java
+++ src/main/java/edu/eci/pgr1/entremente/persistence/imp/DatosBD.java
@@ -10,12 +10,12 @@ package edu.eci.pgr1.entremente.persistence.imp;
  * @author Administrador
  */
 public class DatosBD {
-    public static final String SERVIDOR = "35.194.9.226";
-    public static final String PUERTO = "3306";
-    public static final String USUARIO = "Admin";
-    public static final String PASSWORD = "devSolutionCo2016!";
-    public static final String DRIVER = "com.mysql.jdbc.Driver";
-    public static final String BD = "mysql";
+    public static final String SERVIDOR = "localhost";
+    public static final String PUERTO = "3307";
+    public static final String USUARIO = "root";
+    public static final String PASSWORD = "devSolutionCo2016";
+    public static final String DRIVER = "org.mariadb.jdbc.Driver";
+    public static final String BD = "mariadb";
     public static final String NOMBREDB = "entremente";
     public static final String CONECTOR = "jdbc:"+DatosBD.BD+"://"+DatosBD.SERVIDOR+":"+DatosBD.PUERTO+"/"+DatosBD.NOMBREDB+"?user="+DatosBD.USUARIO+"&password="+DatosBD.PASSWORD;
 }
